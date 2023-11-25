58153bec331fc2760410ae43eabdc8652d8b01f0
David Andrés Uyaguari Jimenez <122131659+David4Andres9@users.noreply.github.com>
2023-11-24 20:37:40 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={"https://pbs.twimg.com/media/DFaOQ06W0AA-O9M.jpg"} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenidos al himalaya
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.puce.edu.ec"
           target="_blank"
           rel="noopener noreferrer"
         >
